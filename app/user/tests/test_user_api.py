f5b819edabbd0a0aa09cd8cb30ccbd7626ce7e81
hasyari <hasyari00@gmail.com>
2024-11-19 14:46:51 +0700

    changes

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -72,19 +72,19 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertIn('token', res.data)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)    
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_bad_credentials(self):
         create_user(email='test@example.com', password='goodpass')
 
         payload = {'email': 'test@example.com', 'password': 'badpass'}
-        res = self.client.post(TOKEN_URL, res.data)
+        res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
-        payload = {'email':'test@example.com', 'password':''}
+        payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
--- app/user/views.py
+++ app/user/views.py
@@ -2,6 +2,6 @@ from rest_framework import generics
 
 from user.serializers import UserSerializer
 
+
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
-    
